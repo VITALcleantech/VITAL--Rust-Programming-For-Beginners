f2525ce5b23fba77e117b60cdb3bbfe9130e263c
VITAL <58122578+VITALcleantech@users.noreply.github.com>
2021-12-07 12:32:41 -0800

    coded a11

--- src/bin/a11.rs
+++ src/bin/a11.rs
@@ -9,4 +9,30 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+
+
+// * Use a struct for the grocery item
+// * Use two i32 fields for the quantity and id number
+struct GroceryItem {
+    quantity: i32,
+    id: i32,
+}
+// * Create a function to display the quantity, with the struct as a parameter
+fn display_quantity(item: &GroceryItem) {
+    println!("quantity: {:?}", item.quantity);
+}
+// * Create a function to display the id number, with the struct as a parameter
+fn display_id(item: &GroceryItem) {
+    println!("id: {:?}", item.id);
+}
+
+fn main() {
+    let my_item = GroceryItem {
+        quantity: 3,
+        id: 99,
+    };
+    display_quantity(&my_item);
+    display_id(&my_item);
+}
+
+

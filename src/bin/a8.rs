932a7b47743e2bb2867ea7100ca4220d10388672
VITAL <58122578+VITALcleantech@users.noreply.github.com>
2021-12-07 10:49:34 -0800

    coded a8

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -9,4 +9,40 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+// * Use an enum to create different flavors of drinks
+enum Alcohol {
+    Bourbon,
+    Beer,
+    Wine,
+}
+// * Use a struct to store drink flavor and fluid ounce information
+struct Drink {
+    flavor: Alcohol,
+    fluid_oz: f64,
+}
+// * Use a function to print out the drink flavor and ounces
+fn print_drink(drink: Drink) {
+    match drink.flavor {
+        Alcohol::Bourbon => println!("flavor: bourbon"),
+        Alcohol::Beer => println!("flavor: beer"),
+        Alcohol::Wine => println!("flavor: wine"),
+    }
+    println!("oz: {:?}", drink.fluid_oz);
+}
+fn main() {
+    let bourbon = Drink {
+        flavor: Alcohol::Bourbon, 
+        fluid_oz: 2.0,
+    };
+    print_drink(bourbon);
+    let beer = Drink {
+        flavor: Alcohol::Beer, 
+        fluid_oz: 16.0,
+    };
+    print_drink(beer);
+    let wine = Drink {
+        flavor: Alcohol::Wine, 
+        fluid_oz: 8.0,
+    };
+    print_drink(wine);
+}

3b96b1b00e63577988ddc5cbc624700ee060a045
VITAL <58122578+VITALcleantech@users.noreply.github.com>
2021-12-07 10:10:45 -0800

    coded a7, runs with comments

--- src/bin/a7.rs
+++ src/bin/a7.rs
@@ -10,4 +10,24 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+
+enum Color {
+    // * Use an enum with color names as variants
+    Red, 
+    Green, 
+    Blue,
+}
+// * Use a function to print the color name
+fn print_color(my_color: Color) {
+    // * Use a match expression to determine which color
+    //   name to print
+    match my_color {
+        Color::Red => println!("Red"),
+        Color::Green => println!("Green"),
+        Color::Blue => println!("Blue"),
+    }
+}
+
+fn main() {
+    print_color(Color::Blue);
+}

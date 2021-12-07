99c3b2bb7731ccd6cb1a7cc816f1e87bf658dc8d
VITAL <58122578+VITALcleantech@users.noreply.github.com>
2021-12-07 07:26:59 -0800

    coded a5

--- src/bin/a5.rs
+++ src/bin/a5.rs
@@ -9,4 +9,18 @@
 // * Print the variable within the loop statement
 // * Use break to exit the loop
 
-fn main() {}
+
+// * Display "1" through "4" in the terminal
+fn main() {
+    // * Use a mutable integer variable
+    let mut i = 1;
+    // * Use a loop statement
+    loop {
+        // * Print the variable within the loop statement
+        println!("{:?}", i);
+        if i == 4 {
+            break;
+        }
+        i = i + 1;
+    }
+}

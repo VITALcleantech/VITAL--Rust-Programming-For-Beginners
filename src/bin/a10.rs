f9252f2543f44f90aa4af4fc58c518f0672330a7
VITAL <58122578+VITALcleantech@users.noreply.github.com>
2021-12-07 11:58:33 -0800

    coded a10

--- src/bin/a10.rs
+++ src/bin/a10.rs
@@ -12,4 +12,23 @@
 // * Use a match expression to determine which message
 //   to print
 
-fn main() {}
+
+fn main() {
+    // * Use a boolean variable set to the result of
+    //   an if..else expression to store whether the value
+    let value = 100;
+    let is_greater_than_100 = value > 100;
+    print_message(is_greater_than_100);
+} 
+
+// * Use a function to print the messages
+fn print_message(greater_than_100: bool) {
+    // * Use a match expression to determine which message
+    //   to print
+    match greater_than_100 {
+        true => println!("it's big"),
+        false => println!("it's small"),
+    }
+}
+
+
